c26698fd34483be7e4a3f31c500fe750e683b4b3
val <val09072010@gmail.com>
2018-07-05 18:13:53 +0300

    o1. Variant with decorator added

--- o1_listconvertor/ListsToDictConvertorsDecorator.py
+++ o1_listconvertor/ListsToDictConvertorsDecorator.py
@@ -4,6 +4,39 @@ import itertools
 
 
 def lists_to_dict(inner):
-    def decor(inner):
-        inner()
+    def decor(*args):
+        values = args[1]
+        keys = args[0]
+        if len(keys) <= len(values):
+            res = dict(zip(keys, values))
+        else:
+            res = inner(*args)
+        return res
     return decor
+
+
+@lists_to_dict
+def lists_to_dict_manual_zip(keys, values):
+    new_values = list(values)
+    new_values = new_values + [None]*(len(keys) - len(values))
+    return dict(zip(keys, new_values))
+
+
+@lists_to_dict
+def lists_to_dict_itertools(keys, values):
+    return dict(itertools.zip_longest(keys, values))  # in Python 2 it was easier -> dict(map(None, keys, vals))  :(
+
+
+def main():
+    keys4 = [1, 2, 3, 4]
+    keys3 = [1, 2, 3]
+    values3 = ['one', 'two', 'three']
+    values4 = ['one', 'two', 'three', 'four']
+    print(lists_to_dict_manual_zip(keys3, values3))
+    print(lists_to_dict_manual_zip(keys3, values4))
+    print(lists_to_dict_manual_zip(keys4, values4))
+    print(lists_to_dict_manual_zip(keys4, values3))
+
+
+if __name__ == '__main__':
+    main()
--- tests/ListsToDictTestCase.py
+++ tests/ListsToDictTestCase.py
@@ -6,18 +6,19 @@ import os
 import unittest
 import o1_listconvertor.ListsToDictConvertersFunc as Tested
 from o1_listconvertor.ListsToDictConvertersClass import ManualListsConverter as ZipLC, MapListsConverter as ItertoolLC
+from o1_listconvertor.ListsToDictConvertorsDecorator import lists_to_dict_itertools, lists_to_dict_manual_zip
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 
-class ListsoDictTestCase(unittest.TestCase):
+class ListsToDictTestCase(unittest.TestCase):
     keys_longer_map = dict({(1, 'one'), (2, 'two'), (3, 'three'), (4, None)})
     lists_eq_map = dict({(1, 'one'), (2, 'two'), (3, 'three'), (4, 'four')})
     values_longer_map = dict({(1, 'one'), (2, 'two'), (3, 'three')})
 
-    keys4 = [1, 2, 3, 4]
-    keys3 = [1, 2, 3]
-    values3 = ['one', 'two', 'three']
-    values4 = ['one', 'two', 'three', 'four']
+    long_keys = [1, 2, 3, 4]
+    short_keys = [1, 2, 3]
+    short_values = ['one', 'two', 'three']
+    long_values = ['one', 'two', 'three', 'four']
 
     zzip = None
     iiter = None
@@ -30,23 +31,29 @@ class ListsoDictTestCase(unittest.TestCase):
         self.zzip = None
         self.iiter = None
 
-    def test_keys_longer_than_vals_functions(self):
-        self.assertEqual(self.keys_longer_map, Tested.lists_to_dict_manual_zip(self.keys4, self.values3))
-        self.assertEqual(self.keys_longer_map, Tested.lists_to_dict_itertools(self.keys4, self.values3))
-        self.assertEqual(self.keys_longer_map, self.zzip.lists_to_dict(self.keys4, self.values3))
-        self.assertEqual(self.keys_longer_map, self.iiter.lists_to_dict(self.keys4, self.values3))
+    def test_keys_longer_than_vals(self):
+        self.assertEqual(self.keys_longer_map, Tested.lists_to_dict_manual_zip(self.long_keys, self.short_values))
+        self.assertEqual(self.keys_longer_map, Tested.lists_to_dict_itertools(self.long_keys, self.short_values))
+        self.assertEqual(self.keys_longer_map, self.zzip.lists_to_dict(self.long_keys, self.short_values))
+        self.assertEqual(self.keys_longer_map, self.iiter.lists_to_dict(self.long_keys, self.short_values))
+        self.assertEqual(self.keys_longer_map, lists_to_dict_manual_zip(self.long_keys, self.short_values))
+        self.assertEqual(self.keys_longer_map, lists_to_dict_itertools(self.long_keys, self.short_values))
 
-    def test_keys_equal_to_vals_functions(self):
-        self.assertEqual(self.lists_eq_map, Tested.lists_to_dict_manual_zip(self.keys4, self.values4))
-        self.assertEqual(self.lists_eq_map, Tested.lists_to_dict_itertools(self.keys4, self.values4))
-        self.assertEqual(self.lists_eq_map, self.zzip.lists_to_dict(self.keys4, self.values4))
-        self.assertEqual(self.lists_eq_map, self.iiter.lists_to_dict(self.keys4, self.values4))
+    def test_keys_equal_to_vals(self):
+        self.assertEqual(self.lists_eq_map, Tested.lists_to_dict_manual_zip(self.long_keys, self.long_values))
+        self.assertEqual(self.lists_eq_map, Tested.lists_to_dict_itertools(self.long_keys, self.long_values))
+        self.assertEqual(self.lists_eq_map, self.zzip.lists_to_dict(self.long_keys, self.long_values))
+        self.assertEqual(self.lists_eq_map, self.iiter.lists_to_dict(self.long_keys, self.long_values))
+        self.assertEqual(self.lists_eq_map, lists_to_dict_manual_zip(self.long_keys, self.long_values))
+        self.assertEqual(self.lists_eq_map, lists_to_dict_itertools(self.long_keys, self.long_values))
 
-    def test_keys_shorter_than_vals_functions(self):
-        self.assertEqual(self.values_longer_map, Tested.lists_to_dict_manual_zip(self.keys3, self.values4))
-        self.assertEqual(self.values_longer_map, Tested.lists_to_dict_itertools(self.keys3, self.values4))
-        self.assertEqual(self.values_longer_map, self.zzip.lists_to_dict(self.keys3, self.values4))
-        self.assertEqual(self.values_longer_map, self.iiter.lists_to_dict(self.keys3, self.values4))
+    def test_keys_shorter_than_vals(self):
+        self.assertEqual(self.values_longer_map, Tested.lists_to_dict_manual_zip(self.short_keys, self.long_values))
+        self.assertEqual(self.values_longer_map, Tested.lists_to_dict_itertools(self.short_keys, self.long_values))
+        self.assertEqual(self.values_longer_map, self.zzip.lists_to_dict(self.short_keys, self.long_values))
+        self.assertEqual(self.values_longer_map, self.iiter.lists_to_dict(self.short_keys, self.long_values))
+        self.assertEqual(self.values_longer_map, lists_to_dict_manual_zip(self.short_keys, self.long_values))
+        self.assertEqual(self.values_longer_map, lists_to_dict_itertools(self.short_keys, self.long_values))
 
 
 if __name__ == '__main__':
